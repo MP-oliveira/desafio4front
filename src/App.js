3017932401ca339063d5f20c087de6f757d2d17c
Alessandra Silva <silvaalejesus@gmail.com>
2020-12-06 16:38:10 -0300

    fix: ajustes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import { Login } from "./pages/PaginaLogin";
 import { Cadastro } from "./pages/PaginaCadastro";
 import { EsqueciSenha } from "./pages/PaginaEscqueci";
 import { AuthContainer } from "./context/auth";
+// import './App.css';
+import { Sidebar } from './components/sidebar';
+import Home from './pages/home';
+import CriarCobranca from './pages/criarCobranca';
 
 export default function App() {
   return (
     <AuthContainer.Provider>
       <BrowserRouter>
         <div className="App">
+
           <Switch>
             <Route exact path="/">
               <Login />
@@ -21,10 +26,25 @@ export default function App() {
             <Route path="/esqueci">
               <EsqueciSenha />
             </Route>
-           
+
             <Route path="/emailSucesso"></Route>
-            <Route path="/home"></Route>
+
+
+            <Route exact path="/home">
+            <Sidebar />
+              <div className="direita">
+                
+                <Home />
+              </div>
+            </Route>
+            <Route exact path="/criar/cobrancas">
+              <div className="direita">
+                <CriarCobranca />
+              </div>
+            </Route>
+
           </Switch>
+
         </div>
       </BrowserRouter>
     </AuthContainer.Provider>
