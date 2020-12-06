607aad022594a4a146068a17150f4d275121b9c1
Alessandra Silva <silvaalejesus@gmail.com>
2020-12-06 16:37:46 -0300

    feat: imagens

---
+++ src/image/index.js
@@ -0,0 +1,7 @@
+import Home_logo from './home.svg'
+import Cobrancas_logo from './cobrancas.svg'
+import Clientes_logo from './clientes.svg'
+import Logo_cubos from './logo.svg'
+import Faturamento_logo from './faturamento.svg'
+
+export default {Home_logo, Cobrancas_logo, Clientes_logo, Logo_cubos, Faturamento_logo};
